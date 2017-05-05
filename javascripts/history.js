ec7155c4b09d42d5c2cc76cfb6231e37dd7a2698
ARIEL REY <ariel.rey@mercadolibre.com>
2017-05-05 15:13:39 -0300

    Adding copy paste button

--- javascripts/history.js
+++ javascripts/history.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     return;
   }
 
+  new Clipboard('.btn');
   var progressBar = $('.progress');
 
   jQuery.ajax({
@@ -41,7 +42,7 @@ $(document).ready(function () {
       var toAdd = '';
 
       for (var j = 0; j < groups[i].length; j++) {
-        toAdd = toAdd + '<div class="col-xs-4"><div class="panel panel-default"><div class="panel-heading">' + groups[i][j].size + '</div><div class="panel-body"><a href="' + groups[i][j].secure_url + '" class="thumbnail" target="_blank"><img src="' + groups[i][j].secure_url + '"></a></div><div class="panel-footer"><div><a href="' + groups[i][j].secure_url + '" target="_blank">Secure</a></div><div><a href="' + groups[i][j].url + '" target="_blank">Not Secure</a></div></div></div></div>';
+        toAdd = toAdd + '<div class="col-xs-12 col-sm-4"><div class="panel panel-default"><div class="panel-heading">' + groups[i][j].size + '</div><div class="panel-body"><a href="' + groups[i][j].secure_url + '" class="thumbnail" target="_blank"><img src="' + groups[i][j].secure_url + '"></a></div><div class="panel-footer"><div class="form"><div class="form-group"><div class="input-group"><input id="image_' + i + '_' + j + '_1" type="text" class="form-control" value="' + groups[i][j].secure_url + '"><div class="input-group-addon"><button class="btn btn-default" data-clipboard-target="#image_' + i + '_' + j + '_1"><img src="images/clippy.svg" width="15"  alt="Copy to clipboard"></button></div></div></div></div><div class="form"><div class="form-group"><div class="input-group"><input id="image_' + i + '_' + j + '_2" type="text" class="form-control" value="' + groups[i][j].url + '"><div class="input-group-addon"><button class="btn btn-default" data-clipboard-target="#image_' + i + '_' + j + '_2"><img src="images/clippy.svg" width="15"  alt="Copy to clipboard"></button></div></div></div></div></div></div></div>';
       }
 
       $('#results').append('<div class="row">' + toAdd + '</div>');
